e057173bfe24bd2afe1798daf7870e12f53386ad
gustavo-garcia-dev <gustavo.gribisilva@gmail.com>
2026-09-01 14:05:29 -0300

    add

--- ribeiro-finance/src/App.jsx
+++ ribeiro-finance/src/App.jsx
@@ -1,9 +1,12 @@
+import Header from "./Components/Header"
 import Sidebar from "./Components/Sidebar"
 import "./Global.css"
 function App() {
   return (
     <div className="container">
       <Sidebar />
+
+      <Header />
     </div>
   )
 }
---
+++ ribeiro-finance/src/Components/Header/index.jsx
@@ -0,0 +1,43 @@
+import "./Header.css"
+
+function Header() {
+  return (
+    <header className="header">
+
+
+      <div className="header-right">
+
+        <div className="search-box">
+          <span>⌕</span>
+          <input
+            type="text"
+            placeholder="Pesquisar..."
+          />
+        </div>
+
+        <button className="header-button">
+          🔔
+        </button>
+
+        <button className="header-button">
+          🌙
+        </button>
+
+        <div className="header-profile">
+          <div className="profile-avatar">
+            G
+          </div>
+
+          <div className="profile-info">
+            <strong>Gustavo</strong>
+            <span>Estudante</span>
+          </div>
+        </div>
+
+      </div>
+
+    </header>
+  );
+}
+
+export default Header;
